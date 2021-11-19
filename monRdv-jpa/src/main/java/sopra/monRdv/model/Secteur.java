46aac66d6f73ff499c929fbf61726ab869f68acc
Estelle Révillion <estelle.revilliion@live.fr>
2021-11-19 17:40:30 +0100

    Avancée praticien

--- monRdv-jpa/src/main/java/sopra/monRdv/model/Secteur.java
+++ monRdv-jpa/src/main/java/sopra/monRdv/model/Secteur.java
@@ -1,5 +1,14 @@
 package sopra.monRdv.model;
 
 public enum Secteur {
-	SECTEUR_1, SECTEUR_2, SECTEUR_3;
+	SECTEUR_1("Secteur 1"), SECTEUR_2("Secteur 2"), SECTEUR_3("Secteur 3");
+	private final String label;
+
+	private Secteur(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
 }
--- monRdv-web/src/main/java/sopra/formation/web/dto/PraticienDTO.java
+++ monRdv-web/src/main/java/sopra/formation/web/dto/PraticienDTO.java
@@ -19,8 +19,8 @@ import sopra.monRdv.model.Secteur;
 import sopra.monRdv.model.Specialite;
 import sopra.monRdv.model.Utilisateur;
 
-public class praticienDTO {
-	
+public class PraticienDTO {
+
 	private Long id;
 	private int version;
 	private Civilite civilite;
@@ -30,22 +30,18 @@ public class praticienDTO {
 	private Secteur secteur;
 	private boolean carteVitale;
 	private String photo;
-	private List<String> paiement;
+	private List<String> paiement = new ArrayList<String>();
 	private boolean carteBancaire;
 	private boolean cheque;
 	private boolean espece;
 	private Integer dureeCreneau;
-	private List<Specialite> specialites = new ArrayList<Specialite>();
-	private List<Creneau> creneaux = new ArrayList<Creneau>();
-	private List<Lieu> lieux = new ArrayList<Lieu>();
+	private List<String> specialites = new ArrayList<String>();
 
-	
-	public praticienDTO() {
+	public PraticienDTO() {
 		super();
 	}
-	
-	
-	public praticienDTO(Praticien praticien) {
+
+	public PraticienDTO(Praticien praticien) {
 		super();
 		this.id = praticien.getId();
 		this.civilite = praticien.getCivilite();
@@ -59,118 +55,140 @@ public class praticienDTO {
 		this.cheque = praticien.isCheque();
 		this.espece = praticien.isEspece();
 		this.dureeCreneau = praticien.getDureeCreneau();
-		this.specialites = praticien.getSpecialites();
-		this.creneaux = praticien.getCreneaux();
-		this.lieux = praticien.getLieux();
-		if(this.carteBancaire) {this.paiement.add("carte Bancaire");}
-		if(this.cheque) {this.paiement.add("cheque");}
-		if(this.espece) {this.paiement.add("espece");}
-	}
 
+		for (Specialite specialite : praticien.getSpecialites()) {
+			this.specialites.add(specialite.getNom());
+		}
+
+		if (this.carteBancaire) {
+			this.paiement.add("carte Bancaire");
+		}
+		if (this.cheque) {
+			this.paiement.add("cheque");
+		}
+		if (this.espece) {
+			this.paiement.add("espece");
+		}
+	}
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public int getVersion() {
 		return version;
 	}
+
 	public void setVersion(int version) {
 		this.version = version;
 	}
+
 	public Civilite getCivilite() {
 		return civilite;
 	}
+
 	public void setCivilite(Civilite civilite) {
 		this.civilite = civilite;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenom() {
 		return prenom;
 	}
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
 	public Secteur getSecteur() {
 		return secteur;
 	}
+
 	public void setSecteur(Secteur secteur) {
 		this.secteur = secteur;
 	}
+
 	public boolean isCarteVitale() {
 		return carteVitale;
 	}
+
 	public void setCarteVitale(boolean carteVitale) {
 		this.carteVitale = carteVitale;
 	}
+
 	public String getPhoto() {
 		return photo;
 	}
+
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
+
 	public List<String> getPaiement() {
 		return paiement;
 	}
+
 	public void setPaiement(List<String> paiement) {
 		this.paiement = paiement;
 	}
+
 	public boolean isCarteBancaire() {
 		return carteBancaire;
 	}
+
 	public void setCarteBancaire(boolean carteBancaire) {
 		this.carteBancaire = carteBancaire;
 	}
+
 	public boolean isCheque() {
 		return cheque;
 	}
+
 	public void setCheque(boolean cheque) {
 		this.cheque = cheque;
 	}
+
 	public boolean isEspece() {
 		return espece;
 	}
+
 	public void setEspece(boolean espece) {
 		this.espece = espece;
 	}
+
 	public Integer getDureeCreneau() {
 		return dureeCreneau;
 	}
+
 	public void setDureeCreneau(Integer dureeCreneau) {
 		this.dureeCreneau = dureeCreneau;
 	}
-	public List<Specialite> getSpecialites() {
+
+	public List<String> getSpecialites() {
 		return specialites;
 	}
-	public void setSpecialites(List<Specialite> specialites) {
+
+	public void setSpecialites(List<String> specialites) {
 		this.specialites = specialites;
 	}
-	public List<Creneau> getCreneaux() {
-		return creneaux;
-	}
-	public void setCreneaux(List<Creneau> creneaux) {
-		this.creneaux = creneaux;
-	}
-	public List<Lieu> getLieux() {
-		return lieux;
-	}
-	public void setLieux(List<Lieu> lieux) {
-		this.lieux = lieux;
-	}
-	
-	
 
 }
