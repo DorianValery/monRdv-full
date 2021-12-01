f75796f70fdaa90a0fc990f58ded418b315800cd
anais <anais.bruno26@gmail.com>
2021-12-01 11:38:59 +0100

    component.ts

--- monRdv-angular/src/app/praticien/praticien.component.ts
+++ monRdv-angular/src/app/praticien/praticien.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-praticien',
-  templateUrl: './praticien.component.html',
-  styleUrls: ['./praticien.component.scss']
-})
-export class PraticienComponent implements OnInit {
+// @Component({
+//   selector: 'app-praticien',
+//   templateUrl: './praticien.component.html',
+//   styleUrls: ['./praticien.component.scss']
+// })
+// export class PraticienComponent implements OnInit {
 
-  constructor() { }
+//  praticienForm: Praticien;
 
-  ngOnInit(): void {
-  }
+//   constructor(private praticienService: PraticienHttpService) { }
 
-}
+//   ngOnInit(): void {
+//   }
+
+//   list(): Array<Evaluation> {
+//     return this.evaluationService.findAll();
+//   }
+
+//   add() {
+//     this.evaluationForm = new Evaluation();
+//   }
+
+//   edit(id: number) {
+//     this.evaluationService.findById(id).subscribe(response => {
+//       this.evaluationForm = response;
+//     }, err => console.log(err));
+//   }
+
+//   save() {
+//     if(this.evaluationForm.id) {
+//       this.evaluationService.modify(this.evaluationForm);
+//     } else {
+//       this.evaluationService.create(this.evaluationForm);
+//     }
+
+//     this.cancel();
+//   }
+
+//   cancel() {
+//     this.evaluationForm = null;
+//   }
+
+//   remove(id: number) {
+//     this.evaluationService.deleteById(id);
+//   }
+// }
+
+// }
