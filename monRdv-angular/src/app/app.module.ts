eaae760ea83e8c46a009d027267fc4e6681542a3
Dorian Valery <d.valery12@gmail.com>
2021-12-01 12:32:30 +0100

    patient-prise-rdv

--- monRdv-angular/src/app/app.module.ts
+++ monRdv-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PraticienComponent } from './praticien/praticien.component';
+import { PatientPriseRdvComponent } from './patient-prise-rdv/patient-prise-rdv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PraticienComponent
+    PatientPriseRdvComponent
   ],
   imports: [
     BrowserModule,
---
+++ monRdv-angular/src/app/patient-prise-rdv/patient-prise-rdv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-prise-rdv',
+  templateUrl: './patient-prise-rdv.component.html',
+  styleUrls: ['./patient-prise-rdv.component.scss']
+})
+export class PatientPriseRdvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
